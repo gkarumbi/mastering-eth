a0f6c9c054c4f9a8b71bd27973fe9b9f7dc131b1
George Karumbi <gkarumbi@gmail.com>
2023-01-30 14:01:11 +0300

    Add contract Owned from which Mortal and Faucet inherits from

--- Faucet2.sol
+++ Faucet2.sol
@@ -1,33 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract Faucet{
+contract Owned{
+    address payable owner;
 
-    address owner ;
-    constructor(){
+    //Contract constructor : set owner
+    constructor()public {
         owner = msg.sender;
+
     }
-    //Accept any incoming amount
-    receive() external payable{}
+
+    //Access control modifier
+    modifier onlyOwner{
+        require(msg.sender == owner,"Only the contract owner can call this function");
+        _;
+    }
+}
+
+contract Mortal is Owned{
+    //contract destructor
+
+    function destroy() public onlyOwner{
+        selfdestruct(owner);
+    }
+}
+
+contract Faucet is Mortal{
+
+   event Withdrawal(address indexed to, uint amount);
+   event Deposit(address indexed from, uint amount);
+    
     //Give out ether to anyone who asks
     function withdraw(uint withdraw_amount) public {
         //Limit withdrwal amount
         require(withdraw_amount <= 0.1 ether);
+        require(address(this).balance >= withdraw_amount, "Insufficient balance in faucet fpr withdrwal request");
 
         //Send the amount to the address tjhhat requested it
         msg.sender.transfer(withdraw_amount);
+        emit Withdrawal(msg.sender, withdraw_amount);
     }
 
-    function destroy() public onlyOwner{
-        
-        selfdestruct(owner);
+    //Accept any incoming amount
+    receive() external payable{
+        emit Deposit(msg.sender, msg.value);
     }
-
     
-    modifier onlyOwner {
-    require(msg.sender == owner);
-    _;
-    }
+    
+    
    
 }
 
