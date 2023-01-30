1956b593c53aa40b0b7797616e9d0fe9fc90cb32
George Karumbi <gkarumbi@gmail.com>
2023-01-30 12:35:21 +0300

    inital code set up

---
+++ Faucet2.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Faucet{
+
+    address owner ;
+    constructor(){
+        owner = msg.sender;
+    }
+    //Accept any incoming amount
+    receive() external payable{}
+    //Give out ether to anyone who asks
+    function withdraw(uint withdraw_amount) public {
+        //Limit withdrwal amount
+        require(withdraw_amount <= 0.1 ether);
+
+        //Send the amount to the address tjhhat requested it
+        msg.sender.transfer(withdraw_amount);
+    }
+
+    function destroy() public{
+        require(msg.sender == owner);
+        selfdestruct(owner);
+    }
+
+   
+}
+
